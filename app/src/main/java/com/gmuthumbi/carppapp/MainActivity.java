73de6f23819c02fc77dbcd65532cd494279d2f42
GloriaMuthumbi <gloria.muthumbi@strathmore.edu>
2020-06-09 21:47:53 +0300

    Initial commit

---
+++ app/src/main/java/com/gmuthumbi/carppapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gmuthumbi.carppapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
